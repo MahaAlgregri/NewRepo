0989b30cb0f275640174e48fe34c50b6ef18d4f8
Maha Algregri <maha.algregri@gmail.com>
2024-01-24 13:55:02 +0300

    This first commit

---
+++ src/Person.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 00019572
+ */
+public class Person {
+    String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+    
+
+    public String getName() {
+        return name;
+    }
+    
+    
+}
---
+++ test/PersonTest.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author 00019572
+ */
+public class PersonTest {
+    
+    public PersonTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getName method, of class Person.
+     */
+    @Test
+    public void testGetName() {
+        System.out.println("getName");
+        Person instance = new Person("Nora");
+        String expResult = "Maha";
+        String result = instance.getName();
+        assertEquals(expResult, result);
+
+    }
+    
+}
